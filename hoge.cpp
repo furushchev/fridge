5d4f09a738ceed2fd92f269ce097d124f0021473
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-12-17 23:18:14 +0900

    add hoge.cpp

---
+++ hoge.cpp
@@ -0,0 +1,12 @@
+/*
+ * hoge.cpp
+ * Author: Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
+ */
+
+#include <iostream>
+
+int main(int argc, char** argv){
+  std::cout << "Hello, World!" << std::endl;
+
+  return 0;
+}
